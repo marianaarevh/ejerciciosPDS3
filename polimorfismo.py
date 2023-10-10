e1a458788b3ec0efe77d923519e8fe4908863adb
marianaarevh <marianaarevalo933@gmail.com>
2023-10-09 20:53:22 -0700

    instrucciones ejercicios polimorfismo

---
+++ polimorfismo.py
@@ -0,0 +1,16 @@
+# Crea una clase llamada "Figura" con un método "calcularArea()" que devuelve un valor predeterminado, como 0.
+# Luego, crea subclases llamadas "Circulo" y "Rectangulo" que hereden de "Figura". 
+# En estas subclases, sobreescríbe el método "calcularArea()" para calcular el área específica de cada figura. 
+# Luego, crea instancias de ambas subclases y llama al método "calcularArea()" en cada una.
+
+
+# Crea una clase llamada "Fruta" con un método "obtenerColor()" que devuelve un color predeterminado, como "desconocido". 
+# Luego, crea subclases llamadas "Manzana" y "Banana" que hereden de "Fruta". 
+# En el programa principal, declara una variable de tipo "Fruta" y asígnale instancias de "Manzana" y "Banana". 
+# Llama al método "obtenerColor()" en ambas instancias y muestra el color de cada fruta. 
+
+
+# Crea una clase llamada "Animal" con un método "mover()" que imprima el movimiento genérico, como "volar" o “nadar”. 
+# Luego, crea subclases llamadas "Perro",  "Pez" y “Pájaro” que hereden de "Animal". 
+# En las subclases, sobreescríbe el método "mover()" para imprimir el movimiento específico para cada animal. 
+# Crea instancias de ambas subclases y llama al método "mover()" en cada una.

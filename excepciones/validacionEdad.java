37f64b807460c45b14bad9ed13a047fb8c1a2523
marianaarevh <marianaarevalo933@gmail.com>
2023-10-14 20:54:19 -0700

    ej. excepciones: instrucciones + imports

---
+++ excepciones/validacionEdad.java
@@ -0,0 +1,8 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/* Ejercicio 1: 
+Validación de Edad Escribe un programa que solicite al usuario ingresar su edad. 
+Utiliza el manejo de excepciones para asegurarte de que la edad ingresada sea un número válido (mayor que 0) 
+y captura cualquier excepción que pueda ocurrir. 
+Muestra un mensaje de error apropiado si se ingresa una edad no válida. */

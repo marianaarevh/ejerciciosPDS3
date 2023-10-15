8b871ba403d6b089490bde4aea2fde8a3e20ae6f
marianaarevh <marianaarevalo933@gmail.com>
2023-10-14 22:07:40 -0700

    ej. Asserts: se agregó la clase calculadora con sus metodos (add, substract, multiply, divide)

---
+++ asserts/Calculadora.java
@@ -0,0 +1,22 @@
+package asserts;
+
+public class Calculadora {
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int substract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide (int a, int b) {
+        if (b==0) {
+            throw new ArithmeticException("Cannot divide by zero");    
+        }
+        return(double) a/b;
+    }
+}
